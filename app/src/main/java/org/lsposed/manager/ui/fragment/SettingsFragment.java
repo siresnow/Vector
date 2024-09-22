JingMatrix/LSPosed

Allow to de-activate log watchlog (#20)
Requested by #14, a switch is added to the release version.
Patch of msvamp rejected in original repository
mywalk added ui interface

Co-authored-by: mywalk <66966897+mywalkb@users.noreply.github.com>

app/src/main/java/org/lsposed/manager/ui/fragment/SettingsFragment.java
@@ -160,6 +160,13 @@ public class SettingsFragment extends BaseFragment {
                 prefVerboseLogs.setOnPreferenceChangeListener((preference, newValue) -> ConfigManager.setVerboseLogEnabled(!(boolean) newValue));
             }
 
+            MaterialSwitchPreference prefEnableLog = findPreference("force_enable_log");
+            if (prefEnableLog != null) {
+                prefEnableLog.setEnabled(!BuildConfig.DEBUG && installed);
+                prefEnableLog.setChecked(!installed || ConfigManager.isLogWatchdogEnabled());
+                prefEnableLog.setOnPreferenceChangeListener((preference, newValue) -> ConfigManager.setLogWatchdog((boolean) newValue));
+            }
+
             MaterialSwitchPreference prefDexObfuscate = findPreference("enable_dex_obfuscate");
             if (prefDexObfuscate != null) {
                 prefDexObfuscate.setEnabled(installed);

daemon/src/main/java/org/lsposed/lspd/service/ConfigManager.java
@@ -99,6 +99,7 @@ public class ConfigManager {
     private final SQLiteDatabase db = openDb();
 
     private boolean verboseLog = true;
+    private boolean logWatchdog = true;
     private boolean dexObfuscate = true;
     private boolean enableStatusNotification = true;
     private Path miscPath = null;
@@ -269,6 +270,9 @@ public class ConfigManager {
 
         Object bool = config.get("enable_verbose_log");
         verboseLog = bool == null || (boolean) bool;
+
+        bool = config.get("force_enable_log");
+        logWatchdog = bool == null || (boolean) bool;
 
         bool = config.get("enable_dex_obfuscate");
         dexObfuscate = bool == null || (boolean) bool;
@@ -1046,6 +1050,21 @@ public class ConfigManager {
 
     public boolean verboseLog() {
         return BuildConfig.DEBUG || verboseLog;
+    }
+
+    public void setLogWatchdog(boolean on) {
+        var logcatService = ServiceManager.getLogcatService();
+        if (on) {
+            logcatService.enableWatchdog();
+        } else {
+            logcatService.disableWatchdog();
+        }
+        updateModulePrefs("lspd", 0, "config", "force_enable_log", on);
+        logWatchdog = on;
+    }
+
+    public boolean isLogWatchdogEnabled() {
+        return logWatchdog;
     }
 
     public void setDexObfuscate(boolean on) {

siresnow/Vector

app/src/main/java/org/lsposed/manager/ui/fragment/SettingsFragment.java
@@ -160,6 +160,13 @@ public class SettingsFragment extends BaseFragment {
                 prefVerboseLogs.setOnPreferenceChangeListener((preference, newValue) -> ConfigManager.setVerboseLogEnabled(!(boolean) newValue));
             }
 
+            MaterialSwitchPreference prefEnableLog = findPreference("enable_log_watchdog");
+            if (prefEnableLog != null) {
+                prefEnableLog.setEnabled(!BuildConfig.DEBUG && installed);
+                prefEnableLog.setChecked(!installed || ConfigManager.isLogWatchdogEnabled());
+                prefEnableLog.setOnPreferenceChangeListener((preference, newValue) -> ConfigManager.setLogWatchdog((boolean) newValue));
+            }
+
             MaterialSwitchPreference prefDexObfuscate = findPreference("enable_dex_obfuscate");
             if (prefDexObfuscate != null) {
                 prefDexObfuscate.setEnabled(installed);

daemon/src/main/java/org/lsposed/lspd/service/ConfigManager.java
@@ -99,6 +99,7 @@ public class ConfigManager {
     private final SQLiteDatabase db = openDb();
 
     private boolean verboseLog = true;
+    private boolean logWatchdog = true;
     private boolean dexObfuscate = true;
     private boolean enableStatusNotification = true;
     private Path miscPath = null;
@@ -269,6 +270,9 @@ public class ConfigManager {
 
         Object bool = config.get("enable_verbose_log");
         verboseLog = bool == null || (boolean) bool;
+
+        bool = config.get("enable_log_watchdog");
+        logWatchdog = bool == null || (boolean) bool;
 
         bool = config.get("enable_dex_obfuscate");
         dexObfuscate = bool == null || (boolean) bool;
@@ -1046,6 +1050,21 @@ public class ConfigManager {
 
     public boolean verboseLog() {
         return BuildConfig.DEBUG || verboseLog;
+    }
+
+    public void setLogWatchdog(boolean on) {
+        var logcatService = ServiceManager.getLogcatService();
+        if (on) {
+            logcatService.enableWatchdog();
+        } else {
+            logcatService.disableWatchdog();
+        }
+        updateModulePrefs("lspd", 0, "config", "enable_log_watchdog", on);
+        logWatchdog = on;
+    }
+
+    public boolean isLogWatchdogEnabled() {
+        return logWatchdog;
     }
 
     public void setDexObfuscate(boolean on) {